3b5fc09ed1dd535292d4ffd679cb8056f7caf93a
atomr <wildkayote@yandex.ru>
2020-08-15 18:07:35 +0500

    Updates

---
+++ precache-manifest.73b01d31e63d3ce926dc81762d52e9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3763dd85299cefc5b40c95b6410e2b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d7d06a378f3db8acef",
+    "url": "/static/css/main.a79bc49d.chunk.css"
+  },
+  {
+    "revision": "e63dd240f0c5b2fb76c3",
+    "url": "/static/js/2.be795898.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.be795898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d7d06a378f3db8acef",
+    "url": "/static/js/main.c5f2dbc1.chunk.js"
+  },
+  {
+    "revision": "be335b808b47fe7ca8a3",
+    "url": "/static/js/runtime-main.5536dc4f.js"
+  }
+]);
